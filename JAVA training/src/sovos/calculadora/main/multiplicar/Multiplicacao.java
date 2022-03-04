1c150f3b78dbec2fba53c8e28dadc6b713008f97
TheusBrun <mateusbrun11@gmail.com>
2022-03-04 15:47:56 -0300

    1°commit - java calculator

---
+++ JAVA training/src/sovos/calculadora/main/multiplicar/Multiplicacao.java
@@ -0,0 +1,40 @@
+package sovos.calculadora.main.multiplicar;
+
+public class Multiplicacao {
+    private int num1;
+    private int num2;
+    private int result;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+    public void mult(){
+        setResult(this.num1 * this.num2);
+    }
+    public void mult(int num1, int num2){
+        setResult(num1 * num2);
+    }
+    public int mult(int num1){
+        return num1 * this.num2;
+    }
+}
